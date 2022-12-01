47519a2f2428839eae7152e1619a36e39a42380b
zimbatm <zimbatm@zimbatm.com>
2022-12-01 23:57:59 +0100

    clean

--- roles/github-actions-runner.nix
+++ roles/github-actions-runner.nix
@@ -1,42 +1,70 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 let
   cfg = config.roles.github-actions-runner;
 in
 {
-  imports = [
-    ../profiles/default.nix
-    ../profiles/cachix-watch-store.nix
-    ../profiles/github-actions-runner.nix
-  ];
-
   options.roles.github-actions-runner = {
-    count = lib.mkOption {
-      
-      description = "Number of github actions runner to deploy";
-      default = 4;
-      types = lib.types.int;
+    url = lib.mkOption {
+      description = "URL of the repo or organization to connect to";
+      type = lib.types.str;
     };
 
-    github-token = "/run/keys/github-token";
+    tokenFile = lib.mkOption {
+      description = "Path to the token";
+      type = lib.types.str;
+    };
+
+    name = lib.mkOption {
+      description = "Prefix name of the runners";
+      type = lib.types.str;
+      default = "github-runner";
+    };
+
+    count = lib.mkOption {
+      description = "Number of github actions runner to deploy";
+      default = 4;
+      type = lib.types.int;
+    };
 
     cachix = {
       cacheName = lib.mkOption {
         description = "Cachix cache name";
       };
+
+      tokenFile = lib.mkOption {
+        description = "Path to the token";
+        type = lib.types.str;
+      };
     };
-
-    cachix-push.enable = true;
-    cachix-push.token = "/run/keys/cachix-token";
-
   };
 
   config = {
+    services.github-runners = builtins.listToAttrs (map
+      (n: rec {
+        name = "${cfg.name}-${toString n}";
+        value = {
+          inherit name;
+          enable = true;
+          url = cfg.url;
+          tokenFile = cfg.tokenFile;
+          serviceOverrides = {
+            DeviceAllow = [ "/dev/kvm" ];
+            PrivateDevices = false;
+            ExtraGroups = [ "kvm" ];
+          };
+          extraPackages = [
+            pkgs.cachix
+            pkgs.nix
+            pkgs.openssh
+          ];
+        };
+      })
+      (lib.range 1 cfg.count));
 
     services.cachix-watch-store = {
       enable = true;
       cacheName = cfg.cachix.cacheName;
-      cachixTokenFile = config.age.secrets.cachixConfig.path;
-      verbose = true;
+      cachixTokenFile = cfg.cachix.tokenFile;
       jobs = 4;
     };
 
