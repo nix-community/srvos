d25b67bd69b2aea0ca7a3a4b7d49cb97b6d1a139
Jörg Thalheim <joerg@thalheim.io>
2022-12-19 16:04:24 +0100

    add some example configuration

--- flake.nix
+++ flake.nix
@@ -1,9 +1,49 @@
 {
   description = "Server-optimized nixos configuration";
 
-  outputs = { ... }: {
+  inputs = {
+    # FIXME: how do we handle multiple releases in future? multiple branches?
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
     nixosModules = {
-      common = ./profiles/default.nix;
+      common = import ./profiles/default.nix;
+    };
+
+    nixosConfigurations = let
+      # some example configuration to make it eval
+      dummy = { config, ... }: {
+        networking.hostName = "example-common";
+        boot.loader.grub.devices = [ "/dev/sda" ];
+        fileSystems."/" = {
+          device = "/dev/sda";
+        };
+        system.stateVersion = config.system.nixos.version;
+        users.users.root.initialPassword = "fnord23";
+      };
+    in {
+      example-common = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          self.nixosModules.common
+          dummy
+        ];
+      };
+
+      example-github-runner = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          self.nixosModules.github-actions-runner
+          dummy
+          {
+            roles.github-actions-runner.cachix.cacheName = "cache-name";
+            roles.github-actions-runner.cachix.tokenFile = "/run/cachix-token-file";
+            roles.github-actions-runner.tokenFile = "/run/gha-token-file";
+            roles.github-actions-runner.url = "https://fixup";
+          }
+        ];
+      };
     };
   };
 }
