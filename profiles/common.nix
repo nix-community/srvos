b66148261e9259b07c2f96eb5e819472dbe5aa7f
Jörg Thalheim <joerg@thalheim.io>
2022-12-29 13:52:17 +0100

    don't force authorizedKeysFiles if git server are enabled

--- profiles/common.nix
+++ profiles/common.nix
@@ -47,7 +47,13 @@
     passwordAuthentication = false;
     useDns = false;
     # Only allow system-level authorized_keys to avoid injections.
-    authorizedKeysFiles = lib.mkForce [ "/etc/ssh/authorized_keys.d/%u" ];
+    # We currently don't enable this when git-based software that relies on this is enabled.
+    # It would be nicer to make it more granular using `Match`.
+    # However those match blocks cannot be put after other `extraConfig` lines
+    # with the current sshd config module, which is however something the sshd
+    # config parser mandates.
+    authorizedKeysFiles = lib.mkIf (!config.services.gitea.enable && !config.services.github.enable && !config.services.gitolite.enable)
+      (lib.mkForce [ "/etc/ssh/authorized_keys.d/%u" ]);
 
     # unbind gnupg sockets if they exists
     extraConfig = "StreamLocalBindUnlink yes";
