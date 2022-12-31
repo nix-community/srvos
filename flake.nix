e3dd501d2b09caa8586e83005f30ef470cd7d2e1
zimbatm <zimbatm@zimbatm.com>
2022-12-31 16:59:47 +0100

    flake: extend test converage
    Hit all the files that we have

--- flake.nix
+++ flake.nix
@@ -11,67 +11,113 @@
 
     nixosConfigurations =
       let
-        # some example configuration to make it eval
-        dummy = { config, ... }: {
-          networking.hostName = "example-common";
+        fake-hardware = {
           boot.loader.grub.devices = [ "/dev/sda" ];
           fileSystems."/" = {
             device = "/dev/sda";
           };
+        };
+        # some example configuration to make it eval
+        dummy = { config, ... }: {
+          networking.hostName = "example-common";
           system.stateVersion = config.system.nixos.version;
           users.users.root.initialPassword = "fnord23";
         };
       in
       {
+        # General
         example-common = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            self.nixosModules.common
             dummy
+            fake-hardware
+            self.nixosModules.common
           ];
         };
         example-server = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            self.nixosModules.server
             dummy
+            fake-hardware
+            self.nixosModules.server
           ];
         };
         example-desktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            self.nixosModules.desktop
             dummy
+            fake-hardware
+            self.nixosModules.desktop
           ];
         };
 
-        example-github-runner = nixpkgs.lib.nixosSystem {
+        # Hardware
+        example-hardware-amazon = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            dummy
+            self.nixosModules.hardware-amazon
+          ];
+        };
+        example-hardware-hetzner-cloud = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            dummy
+            self.nixosModules.hardware-hetzner-cloud
+            {
+              systemd.network.networks."10-uplink".networkConfig.Address = "::cafe:babe:feed:face:dead:beef
+";
+            }
+          ];
+        };
+
+        # Mixins
+        example-mixins-cloud-init = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            dummy
+            fake-hardware
+            self.nixosModules.mixins-cloud-init
+          ];
+        };
+        example-mixins-efi = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            dummy
+            fake-hardware
+            self.nixosModules.mixins-efi
+          ];
+        };
+        example-mixins-telegraf = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            dummy
+            fake-hardware
+            self.nixosModules.mixins-telegraf
+          ];
+        };
+        example-mixins-nginx = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            dummy
+            fake-hardware
+            self.nixosModules.mixins-nginx
+          ];
+        };
+
+        # Roles
+        example-roles-github-actions-runner = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
             self.nixosModules.roles-github-actions-runner
             dummy
+            fake-hardware
             {
               roles.github-actions-runner.cachix.cacheName = "cache-name";
               roles.github-actions-runner.cachix.tokenFile = "/run/cachix-token-file";
               roles.github-actions-runner.tokenFile = "/run/gha-token-file";
               roles.github-actions-runner.url = "https://fixup";
             }
-          ];
-        };
-
-        example-telegraf = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            self.nixosModules.mixins-telegraf
-            dummy
-          ];
-        };
-
-        example-nginx = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            self.nixosModules.mixins-nginx
-            dummy
           ];
         };
       };
