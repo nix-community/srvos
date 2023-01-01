a428827d79da9c8e686db0b25e1b0c32bca4c0ba
Jörg Thalheim <joerg@thalheim.io>
2023-01-01 16:47:46 +0100

    networkd: better defaults to make nixos-rebuild less likely to fail

--- common/networking.nix
+++ common/networking.nix
@@ -11,4 +11,13 @@
   # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
   systemd.services.NetworkManager-wait-online.enable = false;
   systemd.network.wait-online.enable = false;
+
+  # FIXME: Maybe upstream?
+  # Do not take down the network for too long when upgrading,
+  # This also prevents failures of services that are restarted instead of stopped.
+  # It will use `systemctl restart` rather than stopping it with `systemctl stop`
+  # followed by a delayed `systemctl start`.
+  systemd.services.systemd-networkd.stopIfChanged = false;
+  # Services that are only restarted might be not able to resolve when resolved is stopped before
+  systemd.services.systemd-resolved.stopIfChanged = false;
 }
