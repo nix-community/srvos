1ef7bccffc6bf5e4810115eeab63b406fa631cd1
Jörg Thalheim <joerg@thalheim.io>
2023-01-02 10:13:59 +0100

    add tracing module

---
+++ mixins/tracing.nix
@@ -0,0 +1,16 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  programs.bcc.enable = !pkgs.stdenv.hostPlatform.isRiscV;
+  programs.sysdig.enable = !pkgs.stdenv.isAarch64 && !pkgs.stdenv.hostPlatform.isRiscV;
+
+  environment.systemPackages = [
+    pkgs.strace
+
+    # low priority so that we can to use trace from bcc
+    (pkgs.lowPrio config.boot.kernelPackages.perf)
+  ];
+}
