7f27ac682714645c8ff855df2391caf467b0d07f
zimbatm <zimbatm@zimbatm.com>
2023-01-04 17:51:52 +0100

    hardware-hetzner-online: add available kernel modules
    And enable redistributables by default.

--- hardware/hetzner-online/amd.nix
+++ hardware/hetzner-online/amd.nix
@@ -2,6 +2,7 @@
   imports = [ ./. ];
 
   boot.kernelModules = [ "kvm-amd" ];
-  hardware.cpu.amd.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" ];
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
 }
--- hardware/hetzner-online/intel.nix
+++ hardware/hetzner-online/intel.nix
@@ -2,6 +2,7 @@
   imports = [ ./. ];
 
   boot.kernelModules = [ "kvm-intel" ];
-  hardware.cpu.intel.updateMicrocode =
-    lib.mkDefault config.hardware.enableRedistributableFirmware;
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "sd_mod" "nvme" ];
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.enableRedistributableFirmware = lib.mkDefault true;
 }
