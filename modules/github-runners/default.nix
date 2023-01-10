782e1fb02adabc3b3fdaa4f0ca45ac5e33043f58
Jean-François Roche <jfroche@affinitic.be>
2023-01-10 19:03:21 +0100

    Add new profile to create self hosted runners using github app (#6)
    * feat: add original github-runner module
    
    Add original nixpkgs/nixos module to create github runner services.
    
    * feat: use github app to create self hosted runners

---
+++ modules/github-runners/default.nix
@@ -0,0 +1,72 @@
+{ config
+, pkgs
+, lib
+, ...
+}@args:
+
+with lib;
+
+let
+  cfg = config.services.srvos-github-runners;
+in
+{
+  options.services.srvos-github-runners = mkOption {
+    default = { };
+    type = with types; attrsOf (submodule {
+      options = import ./options.nix (args // {
+        # services.github-runners.${name}.name doesn't have a default; it falls back to ${name} below.
+        includeNameDefault = false;
+      });
+    });
+    example = {
+      runner1 = {
+        enable = true;
+        url = "https://github.com/owner/repo";
+        name = "runner1";
+        tokenFile = "/secrets/token1";
+      };
+
+      runner2 = {
+        enable = true;
+        url = "https://github.com/owner/repo";
+        name = "runner2";
+        tokenFile = "/secrets/token2";
+      };
+    };
+    description = lib.mdDoc ''
+      Multiple GitHub Runners.
+    '';
+  };
+
+  config = {
+    assertions =
+      (mapAttrsToList
+        (name: c:
+          {
+            assertion = !(c.tokenFile == null && c.githubApp == null);
+            message = "Missing token file or github app private key file. Specify path either for token in `tokenFile` either for github app private key File in `githubApp.privateKeyFile`";
+          })
+        cfg) ++
+      (mapAttrsToList
+        (name: c:
+          {
+            assertion = !(c.githubApp != null && c.tokenFile != null);
+            message = "${name}:Cannot set both tokenFile and github app private key file. Specify path either for token in `tokenFile` either for github app private key File in `githubApp.privateKeyFile`";
+          })
+        cfg);
+
+    systemd.services = flip mapAttrs' cfg (n: v:
+      let
+        svcName = "github-runner-${n}";
+      in
+      nameValuePair svcName
+        (import ./service.nix (args // {
+          inherit svcName;
+          cfg = v // {
+            name = if v.name != null then v.name else n;
+          };
+          systemdDir = "github-runner/${n}";
+        }))
+    );
+  };
+}
--- roles/github-actions-runner.nix
+++ roles/github-actions-runner.nix
@@ -3,6 +3,10 @@ let
   cfg = config.roles.github-actions-runner;
 in
 {
+  imports = [
+    ../modules/github-runners
+  ];
+
   options.roles.github-actions-runner = {
     url = lib.mkOption {
       description = "URL of the repo or organization to connect to";
@@ -11,7 +15,52 @@ in
 
     tokenFile = lib.mkOption {
       description = "Path to the token";
-      type = lib.types.str;
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+    };
+
+    ephemeral = lib.mkOption {
+      type = lib.types.bool;
+      description = lib.mdDoc ''
+        If enabled, causes the following behavior:
+
+        - Passes the `--ephemeral` flag to the runner configuration script
+        - De-registers and stops the runner with GitHub after it has processed one job
+        - On stop, systemd wipes the runtime directory (this always happens, even without using the ephemeral option)
+        - Restarts the service after its successful exit
+        - On start, wipes the state directory and configures a new runner
+
+        You should only enable this option if `tokenFile` points to a file which contains a
+        personal access token (PAT). If you're using the option with a registration token, restarting the
+        service will fail as soon as the registration token expired.
+      '';
+      default = true;
+    };
+
+
+    githubApp = lib.mkOption {
+      default = null;
+      description = lib.mdDoc ''
+        Authenticate runners using GitHub App
+      '';
+      type = lib.types.nullOr (lib.types.submodule {
+        options = {
+          id = lib.mkOption {
+            type = lib.types.str;
+            description = lib.mdDoc "GitHub App ID";
+          };
+          login = lib.mkOption {
+            type = lib.types.str;
+            description = lib.mdDoc "GitHub login used to register the application";
+          };
+          privateKeyFile = lib.mkOption {
+            type = lib.types.path;
+            description = lib.mdDoc ''
+              The full path to a file containing the GitHub App private key.
+            '';
+          };
+        };
+      });
     };
 
     name = lib.mkOption {
@@ -39,7 +88,7 @@ in
   };
 
   config = {
-    services.github-runners = builtins.listToAttrs (map
+    services.srvos-github-runners = builtins.listToAttrs (map
       (n: rec {
         name = "${cfg.name}-${toString n}";
         value = {
@@ -47,6 +96,8 @@ in
           enable = true;
           url = cfg.url;
           tokenFile = cfg.tokenFile;
+          githubApp = cfg.githubApp;
+          ephemeral = cfg.ephemeral;
           serviceOverrides = {
             DeviceAllow = [ "/dev/kvm" ];
             PrivateDevices = false;
@@ -56,7 +107,9 @@ in
             pkgs.cachix
             pkgs.nix
             pkgs.openssh
+            pkgs.glibc.bin
           ];
+          extraLabels = [ "nix" ];
         };
       })
       (lib.range 1 cfg.count));
