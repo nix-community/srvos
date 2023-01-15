ebbec71d5c2e5a7c8d28f1f0a561105ba7e33200
Jörg Thalheim <joerg@thalheim.io>
2023-01-15 17:49:24 +0100

    serial: resize terminal after login on serial consoles and enable colors
    Update common/serial.nix
    
    Co-authored-by: Jonas Chevalier <zimbatm@zimbatm.com>

--- common/serial.nix
+++ common/serial.nix
@@ -1,4 +1,29 @@
 { config, lib, pkgs, ... }:
+let
+    # Based on https://unix.stackexchange.com/questions/16578/resizable-serial-console-window
+  resize = pkgs.writeScriptBin "resize" ''
+    export PATH=${pkgs.coreutils}/bin
+    if [ ! -t 0 ]; then
+      # not a interactive...
+      exit 0
+    fi
+    TTY="$(tty)"
+    if [[ "$TTY" != /dev/ttyS* ]] && [[ "$TTY" != /dev/ttyAMA* ]] && [[ "$TTY" != /dev/ttySIF* ]]; then
+      # probably not a known serial console, we could make this check more
+      # precise by using `setserial` but this would require some additional
+      # dependency
+      exit 0
+    fi
+    old=$(stty -g)
+    stty raw -echo min 0 time 5
+
+    printf '\0337\033[r\033[999;999H\033[6n\0338' > /dev/tty
+    IFS='[;R' read -r _ rows cols _ < /dev/tty
+
+    stty "$old"
+    stty cols "$cols" rows "$rows"
+  '';
+in
 {
 
   options = {
@@ -21,13 +46,22 @@
         options on the kernel command line, which will result in output appearing on all
         of them. The last device specified will be used when opening /dev/console. This
         information can be found in the Linux documentation at
-        https://www.kernel.org/doc/html/v4.14/admin-guide/serial-console.html.
+        <https://www.kernel.org/doc/html/v4.14/admin-guide/serial-console.html>.
       '';
     };
   };
 
   config = {
     boot.kernelParams = map (c: "console=${c}") config.srvos.boot.consoles;
+
+    # set terminal size once after login
+    environment.loginShellInit = "${resize}/bin/resize";
+
+    # allows user to change terminal size when it changed locally
+    environment.systemPackages = [ resize ];
+
+    # default is something like vt220... however we want to get alt least some colors...
+    systemd.services."serial-getty@".environment.TERM = "xterm-256color";
 
     # also make grub respond on serial consoles
     boot.loader.grub.extraConfig = ''
