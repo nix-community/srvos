b9e71438a3500955084ff77e58e7d5ca2c578afb
zimbatm <zimbatm@zimbatm.com>
2023-04-06 16:38:16 +0200

    well-known-hosts: use hostname as keys

--- nixos/common/well-known-hosts.nix
+++ nixos/common/well-known-hosts.nix
@@ -2,13 +2,13 @@
 {
   # Avoid TOFU MITM with github by providing their public key here.
   programs.ssh.knownHosts = {
-    github-ed25519.hostNames = [ "github.com" ];
-    github-ed25519.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    "github.com".hostNames = [ "github.com" ];
+    "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
 
-    gitlab-ed25519.hostNames = [ "gitlab.com" ];
-    gitlab-ed25519.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+    "gitlab.com".hostNames = [ "gitlab.com" ];
+    "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
 
-    sourcehut-ed25519.hostNames = [ "git.sr.ht" ];
-    sourcehut-ed25519.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
+    "git.sr.ht".hostNames = [ "git.sr.ht" ];
+    "git.sr.ht".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMZvRd4EtM7R+IHVMWmDkVU3VLQTSwQDSAvW0t2Tkj60";
   };
 }
