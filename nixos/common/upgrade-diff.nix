840296d9eb3ce4dfd3514a91d84f87ed60db76aa
phaer <hello@phaer.org>
2023-06-13 22:49:02 +0200

    Run diff activationScript on dry-activate...
    This is safe as the script doesn't modify the system.
    And it's useful to check before applying.

--- nixos/common/upgrade-diff.nix
+++ nixos/common/upgrade-diff.nix
@@ -1,9 +1,12 @@
 # MIT Jörg Thalheim - https://github.com/Mic92/dotfiles/blob/c6cad4e57016945c4816c8ec6f0a94daaa0c3203/nixos/modules/upgrade-diff.nix
 { pkgs, ... }:
 {
-  system.activationScripts.diff = ''
+  system.activationScripts.diff = {
+    supportsDryActivation = true;
+    text = ''
     if [[ -e /run/current-system ]]; then
       ${pkgs.nix}/bin/nix --extra-experimental-features nix-command store diff-closures /run/current-system "$systemConfig"
     fi
   '';
+  };
 }
