762038b99a08bbfc45d115f68e529de676d8a7a1
zimbatm <zimbatm@zimbatm.com>
2023-06-19 15:27:54 +0200

    ci: use GHA
    Stop using both Garnix and GHA

--- checks/default.nix
+++ checks/default.nix
@@ -1,25 +1,6 @@
 { self, pkgs, system }:
 let
   inherit (pkgs) lib;
-  join = lib.concatStringsSep;
-  keys = lib.attrNames;
-
-  borsChecks =
-    map (name: ''  "check ${name} [${system}]"'') (keys self.checks.${system} or { })
-    ++
-    map (name: ''  "nixosConfig ${name}"'') (keys self.nixosConfigurations or { })
-    ++
-    map (name: ''  "package ${name} [${system}]"'') (keys self.packages.${system} or { })
-  ;
-
-  borsTOML = pkgs.writeText "bors.toml" ''
-    # Generated with ./bors.toml.sh
-    cut_body_after = "" # don't include text from the PR body in the merge commit message
-    status = [
-      "Evaluate flake.nix",
-      ${join ",\n  " borsChecks},
-    ]
-  '';
 
   nixosTest = import "${pkgs.path}/nixos/lib/testing-python.nix" { inherit system pkgs; };
 
@@ -47,22 +28,4 @@ let
       (name: value: { name = "nixos-${name}"; value = value.config.system.build.toplevel; })
       sysConfigs;
 in
-{
-  # Check if the bors.toml needs to be updated
-  testBorsTOML = pkgs.runCommand
-    "test-bors-toml"
-    {
-      passthru.borsTOML = borsTOML;
-    }
-    ''
-      if ! diff ${../bors.toml} ${borsTOML}; then
-        echo "The generated ./bors.toml is out of sync"
-        echo "Run ./bors.toml.sh to fix the issue"
-        exit 1
-      fi
-      touch $out
-    '';
-
-}
-// (lib.optionalAttrs pkgs.stdenv.isLinux moduleTests)
-  // nixosChecks
+nixosChecks // (lib.optionalAttrs pkgs.stdenv.isLinux moduleTests)
