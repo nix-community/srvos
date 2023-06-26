a8d91908c35b12448abe3b3dfc48b2a2453c5a6d
phaer <hello@phaer.org>
2023-06-26 14:04:59 +0200

    telegraf: refactor exec checks

--- nixos/mixins/telegraf.nix
+++ nixos/mixins/telegraf.nix
@@ -7,6 +7,72 @@ let
   isVM = lib.any (mod: mod == "xen-blkfront" || mod == "virtio_console") config.boot.initrd.kernelModules;
   # potentially wrong if the nvme is not used at boot...
   hasNvme = lib.any (m: m == "nvme") config.boot.initrd.availableKernelModules;
+
+  ipv6DadCheck = pkgs.writeShellScript "ipv6-dad-check" ''
+    ${pkgs.iproute2}/bin/ip --json addr | \
+    ${pkgs.jq}/bin/jq -r 'map(.addr_info) | flatten(1) | map(select(.dadfailed == true)) | map(.local) | @text "ipv6_dad_failures count=\(length)i"'
+  '';
+
+  zfsChecks = lib.optional
+    (lib.any (fs: fs == "zfs") config.boot.supportedFilesystems)
+    (pkgs.writeScript "zpool-health" ''
+      #!${pkgs.gawk}/bin/awk -f
+      BEGIN {
+        while ("${pkgs.zfs}/bin/zpool status" | getline) {
+          if ($1 ~ /pool:/) { printf "zpool_status,name=%s ", $2 }
+          if ($1 ~ /state:/) { printf " state=\"%s\",", $2 }
+          if ($1 ~ /errors:/) {
+              if (index($2, "No")) printf "errors=0i\n"; else printf "errors=%di\n", $2
+          }
+        }
+      }
+    '');
+
+  nfsChecks =
+    let
+      collectHosts = shares: fs:
+        if builtins.elem fs.fsType [ "nfs" "nfs3" "nfs4" ]
+        then
+          shares
+          // (
+            let
+              # also match ipv6 addresses
+              group = builtins.match "\\[?([^\]]+)]?:([^:]+)$" fs.device;
+              host = builtins.head group;
+              path = builtins.elemAt group 1;
+            in
+            {
+              ${host} = (shares.${host} or [ ]) ++ [ path ];
+            }
+          )
+        else shares;
+      nfsHosts = lib.foldl collectHosts { } (builtins.attrValues config.fileSystems);
+    in
+    lib.mapAttrsToList
+      (
+        host: args:
+          (pkgs.writeScript "nfs-health" ''
+            #!${pkgs.gawk}/bin/awk -f
+            BEGIN {
+              for (i = 2; i < ARGC; i++) {
+                  mounts[ARGV[i]] = 1
+              }
+              while ("${pkgs.nfs-utils}/bin/showmount -e " ARGV[1] | getline) {
+                if (NR == 1) { continue }
+                if (mounts[$1] == 1) {
+                    printf "nfs_export,host=%s,path=%s present=1\n", ARGV[1], $1
+                }
+                delete mounts[$1]
+              }
+              for (mount in mounts) {
+                  printf "nfs_export,host=%s,path=%s present=0\n", ARGV[1], $1
+              }
+            }
+          '')
+          + " ${host} ${builtins.concatStringsSep " " args}"
+      )
+      nfsHosts;
+
 in
 {
 
@@ -47,72 +113,9 @@ in
           {
             ## Commands array
             commands =
-              [
-                (pkgs.writeShellScript "ipv6-dad-check" ''
-                  ${pkgs.iproute2}/bin/ip --json addr | \
-                    ${pkgs.jq}/bin/jq -r 'map(.addr_info) | flatten(1) | map(select(.dadfailed == true)) | map(.local) | @text "ipv6_dad_failures count=\(length)i"'
-                '')
-              ]
-              ++ (
-                lib.optional (lib.any (fs: fs == "zfs") config.boot.supportedFilesystems)
-                  (pkgs.writeScript "zpool-health" ''
-                    #!${pkgs.gawk}/bin/awk -f
-                    BEGIN {
-                      while ("${pkgs.zfs}/bin/zpool status" | getline) {
-                        if ($1 ~ /pool:/) { printf "zpool_status,name=%s ", $2 }
-                        if ($1 ~ /state:/) { printf " state=\"%s\",", $2 }
-                        if ($1 ~ /errors:/) {
-                            if (index($2, "No")) printf "errors=0i\n"; else printf "errors=%di\n", $2
-                        }
-                      }
-                    }
-                  '')
-              )
-              ++ (
-                let
-                  collectHosts = shares: fs:
-                    if builtins.elem fs.fsType [ "nfs" "nfs3" "nfs4" ]
-                    then
-                      shares
-                        // (
-                        let
-                          # also match ipv6 addresses
-                          group = builtins.match "\\[?([^\]]+)]?:([^:]+)$" fs.device;
-                          host = builtins.head group;
-                          path = builtins.elemAt group 1;
-                        in
-                        {
-                          ${host} = (shares.${host} or [ ]) ++ [ path ];
-                        }
-                      )
-                    else shares;
-                  nfsHosts = lib.foldl collectHosts { } (builtins.attrValues config.fileSystems);
-                in
-                lib.mapAttrsToList
-                  (
-                    host: args:
-                      (pkgs.writeScript "nfs-health" ''
-                        #!${pkgs.gawk}/bin/awk -f
-                        BEGIN {
-                          for (i = 2; i < ARGC; i++) {
-                              mounts[ARGV[i]] = 1
-                          }
-                          while ("${pkgs.nfs-utils}/bin/showmount -e " ARGV[1] | getline) {
-                            if (NR == 1) { continue }
-                            if (mounts[$1] == 1) {
-                                printf "nfs_export,host=%s,path=%s present=1\n", ARGV[1], $1
-                            }
-                            delete mounts[$1]
-                          }
-                          for (mount in mounts) {
-                              printf "nfs_export,host=%s,path=%s present=0\n", ARGV[1], $1
-                          }
-                        }
-                      '')
-                      + " ${host} ${builtins.concatStringsSep " " args}"
-                  )
-                  nfsHosts
-              );
+              [ ipv6DadCheck ]
+                ++ zfsChecks
+                ++ nfsChecks;
             data_format = "influx";
           }
         ];
