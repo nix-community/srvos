f3e1911bc26187ea52a5fb92a793a34a532fbcc7
Jonas Chevalier <zimbatm@zimbatm.com>
2023-08-14 17:31:06 +0200

    Revert "common: pin to nix to v2.16 for now" (#219)
    nixpkgs now defaults to nix v2.17
    
    This reverts commit f61c861e67ebac21b105dfe32c72c4fc19e3678c introduced
    in #208

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -1,8 +1,5 @@
-{ lib, pkgs, ... }:
+{ lib, ... }:
 {
-  # Use a version of Nix that works
-  nix.package = lib.mkDefault pkgs.nixVersions.nix_2_16;
-
   # Fallback quickly if substituters are not available.
   nix.settings.connect-timeout = 5;
 
