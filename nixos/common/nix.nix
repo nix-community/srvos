00f20046801d8b802684d9c878a49fe46133a8a8
Jörg Thalheim <joerg@thalheim.io>
2023-09-05 09:41:23 +0200

    nix-gc: update min-free/max-free for the year 2023
    Even on small vps you don't want to have less than 3GB in my opionion

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -13,8 +13,8 @@
   nix.settings.log-lines = lib.mkDefault 25;
 
   # Avoid disk full issues
-  nix.settings.max-free = lib.mkDefault (1000 * 1000 * 1000);
-  nix.settings.min-free = lib.mkDefault (128 * 1000 * 1000);
+  nix.settings.max-free = lib.mkDefault (3000 * 1024 * 1024);
+  nix.settings.min-free = lib.mkDefault (512 * 1024 * 1024);
 
   # TODO: cargo culted.
   nix.daemonCPUSchedPolicy = lib.mkDefault "batch";
