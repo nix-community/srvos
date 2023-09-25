e66b4f9084f515ada8ee327b4c2410863f55e244
zowoq <59103226+zowoq@users.noreply.github.com>
2023-09-25 16:09:17 +1000

    upgrade-diff: switch to `nvd`
    extra-experimental-features is set automatically

--- nixos/common/upgrade-diff.nix
+++ nixos/common/upgrade-diff.nix
@@ -1,12 +1,12 @@
 # MIT Jörg Thalheim - https://github.com/Mic92/dotfiles/blob/c6cad4e57016945c4816c8ec6f0a94daaa0c3203/nixos/modules/upgrade-diff.nix
-{ config, ... }:
+{ pkgs, ... }:
 {
   system.activationScripts.diff = {
     supportsDryActivation = true;
     text = ''
       if [[ -e /run/current-system ]]; then
         echo "--- diff to current-system"
-        ${config.nix.package}/bin/nix --extra-experimental-features nix-command store diff-closures /run/current-system "$systemConfig"
+        ${pkgs.nvd}/bin/nvd diff /run/current-system "$systemConfig"
         echo "---"
       fi
     '';
