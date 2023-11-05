5b5984e895c67d61c7592cf675f3f15e9a06f65b
zowoq <59103226+zowoq@users.noreply.github.com>
2023-11-06 09:13:29 +1000

    move nixos tests to main flake
    also disable tests on aarch64-linux

--- flake.nix
+++ flake.nix
@@ -3,7 +3,17 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
-  outputs = { self, ... }:
+  inputs.nixos-23_05.url = "github:NixOS/nixpkgs/nixos-23.05";
+
+  outputs = { nixpkgs, nixos-23_05, self, ... }:
+    let
+      srvos = self;
+      inherit (nixpkgs) lib;
+      eachSystem = f:
+        lib.genAttrs
+          srvos.lib.supportedSystems
+          (system: f nixpkgs.legacyPackages.${system});
+    in
     {
       lib.supportedSystems = [
         "aarch64-darwin"
@@ -11,6 +21,18 @@
         "x86_64-darwin"
         "x86_64-linux"
       ];
+
+      checks = eachSystem (pkgs:
+        { } // (lib.optionalAttrs (pkgs.system == "x86_64-linux") (import ./dev/checks.nix {
+          inherit srvos nixpkgs;
+          prefix = "nixos";
+          system = pkgs.system;
+        })) // (lib.optionalAttrs (pkgs.system == "x86_64-linux") (import ./dev/checks.nix {
+          inherit srvos;
+          nixpkgs = nixos-23_05;
+          prefix = "nixos-23_05";
+          system = pkgs.system;
+        })));
 
       # generates future flake outputs: `modules.<kind>.<module-name>`
       modules.nixos = import ./nixos;
