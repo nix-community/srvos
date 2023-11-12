533a8681bb4d97e2c04378aa76aa1836773bb18a
Jörg Thalheim <Mic92@users.noreply.github.com>
2023-11-12 11:27:36 +0100

    sudo: drop explicit sudo enable (#304)
    This makes it harder to replace sudo with sudo-rs

--- nixos/common/sudo.nix
+++ nixos/common/sudo.nix
@@ -1,6 +1,4 @@
 {
-  # Allow sudo from the @wheel group
-  security.sudo.enable = true;
   # Only allow members of the wheel group to execute sudo by setting the executable’s permissions accordingly. This prevents users that are not members of wheel from exploiting vulnerabilities in sudo such as CVE-2021-3156.
   security.sudo.execWheelOnly = true;
   # Don't lecture the user. Less mutable state.
