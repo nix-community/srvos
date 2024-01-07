aab699142f781c887ca173c9efc65f6ac7db47a6
zowoq <59103226+zowoq@users.noreply.github.com>
2024-01-07 18:52:14 +1000

    checks: run on aarch64-linux

--- dev/checks.nix
+++ dev/checks.nix
@@ -29,4 +29,4 @@ let
       (name: value: { name = "${prefix}-${name}"; value = value.config.system.build.toplevel; })
       configurations;
 in
-nixosChecks // pkgs.lib.optionalAttrs (pkgs.system == "x86_64-linux") moduleTests
+nixosChecks // moduleTests
