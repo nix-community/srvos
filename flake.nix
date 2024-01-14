f69ed55135b52113f395c423204b578d3b80aaf8
zowoq <59103226+zowoq@users.noreply.github.com>
2024-01-14 10:24:07 +1000

    flake: add nix-community cachix nixConfig

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "Server-optimized NixOS configuration";
 
+  nixConfig.extra-substituters = [
+    "https://nix-community.cachix.org"
+  ];
+  nixConfig.extra-trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
   outputs = publicInputs @ { self, nixpkgs, ... }:
