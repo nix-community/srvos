8d159ac5bb67368509861cf1a94717402d8d216e
zowoq <59103226+zowoq@users.noreply.github.com>
2024-02-23 18:27:52 +1000

    no stub (#387)
    * Revert "no stub: <insert TLC song> (#383)"
    
    This reverts commit c26b531a3c4f568c22588983781eb308b615f6ce.
    
    * no stub

--- nixos/server/default.nix
+++ nixos/server/default.nix
@@ -1,15 +1,15 @@
 # A default configuration that applies to all servers.
 # Common configuration accross *all* the machines
-{ options, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 {
 
   imports = [
     ../common
   ];
 
-  config = {
+  environment = {
     # List packages installed in system profile.
-    environment.systemPackages = map lib.lowPrio [
+    systemPackages = map lib.lowPrio [
       pkgs.curl
       pkgs.dnsutils
       pkgs.gitMinimal
@@ -17,79 +17,78 @@
       pkgs.jq
       pkgs.tmux
     ];
-
-    # Notice this also disables --help for some commands such es nixos-rebuild
-    documentation.enable = lib.mkDefault false;
-    documentation.info.enable = lib.mkDefault false;
-    documentation.man.enable = lib.mkDefault false;
-    documentation.nixos.enable = lib.mkDefault false;
-
-    # No need for fonts on a server
-    fonts.fontconfig.enable = lib.mkDefault false;
-
-    programs.vim.defaultEditor = lib.mkDefault true;
-
     # Print the URL instead on servers
-    environment.variables.BROWSER = "echo";
-
-    # Make sure firewall is enabled
-    networking.firewall.enable = true;
-
-    # Delegate the hostname setting to dhcp/cloud-init by default
-    networking.hostName = lib.mkDefault "";
-
-    # If the user is in @wheel they are trusted by default.
-    nix.settings.trusted-users = [ "root" "@wheel" ];
-
-    security.sudo.wheelNeedsPassword = false;
-
-    # Enable SSH everywhere
-    services.openssh.enable = true;
-
-    # No need for sound on a server
-    sound.enable = false;
-
-    # UTC everywhere!
-    time.timeZone = lib.mkDefault "UTC";
-
-    # No mutable users by default
-    users.mutableUsers = false;
-
-    systemd = {
-      # Given that our systems are headless, emergency mode is useless.
-      # We prefer the system to attempt to continue booting so
-      # that we can hopefully still access it remotely.
-      enableEmergencyMode = false;
-
-      # For more detail, see:
-      #   https://0pointer.de/blog/projects/watchdog.html
-      watchdog = {
-        # systemd will send a signal to the hardware watchdog at half
-        # the interval defined here, so every 10s.
-        # If the hardware watchdog does not get a signal for 20s,
-        # it will forcefully reboot the system.
-        runtimeTime = "20s";
-        # Forcefully reboot if the final stage of the reboot
-        # hangs without progress for more than 30s.
-        # For more info, see:
-        #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
-        rebootTime = "30s";
-      };
-
-      sleep.extraConfig = ''
-        AllowSuspend=no
-        AllowHibernation=no
-      '';
-    };
-
-    # use TCP BBR has significantly increased throughput and reduced latency for connections
-    boot.kernel.sysctl = {
-      "net.core.default_qdisc" = "fq";
-      "net.ipv4.tcp_congestion_control" = "bbr";
-    };
-  } // lib.optionalAttrs (options.environment ? stub-ld) {
+    variables.BROWSER = "echo";
+  } // lib.optionalAttrs (lib.versionAtLeast (lib.versions.majorMinor lib.version) "24.05") {
     # Don't install the /lib/ld-linux.so.2 and /lib64/ld-linux-x86-64.so.2
     # stubs. Server users should know what they are doing.
-    environment.stub-ld.enable = lib.mkDefault false;
+    stub-ld.enable = lib.mkDefault false;
+  };
+
+  # Notice this also disables --help for some commands such es nixos-rebuild
+  documentation.enable = lib.mkDefault false;
+  documentation.info.enable = lib.mkDefault false;
+  documentation.man.enable = lib.mkDefault false;
+  documentation.nixos.enable = lib.mkDefault false;
+
+  # No need for fonts on a server
+  fonts.fontconfig.enable = lib.mkDefault false;
+
+  programs.vim.defaultEditor = lib.mkDefault true;
+
+  # Make sure firewall is enabled
+  networking.firewall.enable = true;
+
+  # Delegate the hostname setting to dhcp/cloud-init by default
+  networking.hostName = lib.mkDefault "";
+
+  # If the user is in @wheel they are trusted by default.
+  nix.settings.trusted-users = [ "root" "@wheel" ];
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # Enable SSH everywhere
+  services.openssh.enable = true;
+
+  # No need for sound on a server
+  sound.enable = false;
+
+  # UTC everywhere!
+  time.timeZone = lib.mkDefault "UTC";
+
+  # No mutable users by default
+  users.mutableUsers = false;
+
+  systemd = {
+    # Given that our systems are headless, emergency mode is useless.
+    # We prefer the system to attempt to continue booting so
+    # that we can hopefully still access it remotely.
+    enableEmergencyMode = false;
+
+    # For more detail, see:
+    #   https://0pointer.de/blog/projects/watchdog.html
+    watchdog = {
+      # systemd will send a signal to the hardware watchdog at half
+      # the interval defined here, so every 10s.
+      # If the hardware watchdog does not get a signal for 20s,
+      # it will forcefully reboot the system.
+      runtimeTime = "20s";
+      # Forcefully reboot if the final stage of the reboot
+      # hangs without progress for more than 30s.
+      # For more info, see:
+      #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
+      rebootTime = "30s";
+    };
+
+    sleep.extraConfig = ''
+      AllowSuspend=no
+      AllowHibernation=no
+    '';
+  };
+
+  # use TCP BBR has significantly increased throughput and reduced latency for connections
+  boot.kernel.sysctl = {
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_congestion_control" = "bbr";
   };
 }
