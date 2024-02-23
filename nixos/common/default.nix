cd20518f8832094b06a78c8629873c298d816a66
zowoq <59103226+zowoq@users.noreply.github.com>
2024-02-23 16:36:33 +1000

    Revert "no stub: <insert TLC song> (#383)"
    This reverts commit c26b531a3c4f568c22588983781eb308b615f6ce.

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -1,7 +1,8 @@
 # A default configuration that applies to all servers.
 # Common configuration accross *all* the machines
-{ config, options, lib, ... }:
+{ config, lib, ... }:
 {
+
   imports = [
     ./flake.nix
     ./mdmonitor-fix.nix
@@ -15,33 +16,26 @@
     ./zfs.nix
   ];
 
-  config = {
-    # Use systemd during boot as well on systems except:
-    # - systems that require networking in early-boot
-    # - systems with raids as this currently require manual configuration (https://github.com/NixOS/nixpkgs/issues/210210)
-    # - for containers we currently rely on the `stage-2` init script that sets up our /etc
-    boot.initrd.systemd.enable = lib.mkDefault (
-      !(if lib.versionAtLeast (lib.versions.majorMinor lib.version) "23.11" then
-        config.boot.swraid.enable
-      else
-        config.boot.initrd.services.swraid.enable) &&
-      !config.boot.isContainer &&
-      !config.boot.growPartition
-    );
+  # Use systemd during boot as well on systems except:
+  # - systems that require networking in early-boot
+  # - systems with raids as this currently require manual configuration (https://github.com/NixOS/nixpkgs/issues/210210)
+  # - for containers we currently rely on the `stage-2` init script that sets up our /etc
+  boot.initrd.systemd.enable = lib.mkDefault (
+    !(if lib.versionAtLeast (lib.versions.majorMinor lib.version) "23.11" then
+      config.boot.swraid.enable
+    else
+      config.boot.initrd.services.swraid.enable) &&
+    !config.boot.isContainer &&
+    !config.boot.growPartition
+  );
 
-    # Work around for https://github.com/NixOS/nixpkgs/issues/124215
-    documentation.info.enable = false;
+  # Work around for https://github.com/NixOS/nixpkgs/issues/124215
+  documentation.info.enable = false;
 
-    # This is pulled in by the container profile, but it seems broken and causes
-    # unecessary rebuilds.
-    environment.noXlibs = false;
+  # This is pulled in by the container profile, but it seems broken and causes
+  # unecessary rebuilds.
+  environment.noXlibs = false;
 
-    # Ensure a clean & sparkling /tmp on fresh boots.
-    boot.tmp.cleanOnBoot = lib.mkDefault true;
-  } // lib.optionalAttrs (options.environment ? ldso32) {
-    # Don't install the /lib/ld-linux.so.2 stub. This saves one instance of
-    # nixpkgs.
-    environment.ldso32 = null;
-  };
-
+  # Ensure a clean & sparkling /tmp on fresh boots.
+  boot.tmp.cleanOnBoot = lib.mkDefault true;
 }
--- nixos/server/default.nix
+++ nixos/server/default.nix
@@ -1,95 +1,89 @@
 # A default configuration that applies to all servers.
 # Common configuration accross *all* the machines
-{ options, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 {
 
   imports = [
     ../common
   ];
 
-  config = {
-    # List packages installed in system profile.
-    environment.systemPackages = map lib.lowPrio [
-      pkgs.curl
-      pkgs.dnsutils
-      pkgs.gitMinimal
-      pkgs.htop
-      pkgs.jq
-      pkgs.tmux
-    ];
+  # List packages installed in system profile.
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.dnsutils
+    pkgs.gitMinimal
+    pkgs.htop
+    pkgs.jq
+    pkgs.tmux
+  ];
 
-    # Notice this also disables --help for some commands such es nixos-rebuild
-    documentation.enable = lib.mkDefault false;
-    documentation.info.enable = lib.mkDefault false;
-    documentation.man.enable = lib.mkDefault false;
-    documentation.nixos.enable = lib.mkDefault false;
+  # Notice this also disables --help for some commands such es nixos-rebuild
+  documentation.enable = lib.mkDefault false;
+  documentation.info.enable = lib.mkDefault false;
+  documentation.man.enable = lib.mkDefault false;
+  documentation.nixos.enable = lib.mkDefault false;
 
-    # No need for fonts on a server
-    fonts.fontconfig.enable = lib.mkDefault false;
+  # No need for fonts on a server
+  fonts.fontconfig.enable = lib.mkDefault false;
 
-    programs.vim.defaultEditor = lib.mkDefault true;
+  programs.vim.defaultEditor = lib.mkDefault true;
 
-    # Print the URL instead on servers
-    environment.variables.BROWSER = "echo";
+  # Print the URL instead on servers
+  environment.variables.BROWSER = "echo";
 
-    # Make sure firewall is enabled
-    networking.firewall.enable = true;
+  # Make sure firewall is enabled
+  networking.firewall.enable = true;
 
-    # Delegate the hostname setting to dhcp/cloud-init by default
-    networking.hostName = lib.mkDefault "";
+  # Delegate the hostname setting to dhcp/cloud-init by default
+  networking.hostName = lib.mkDefault "";
 
-    # If the user is in @wheel they are trusted by default.
-    nix.settings.trusted-users = [ "root" "@wheel" ];
+  # If the user is in @wheel they are trusted by default.
+  nix.settings.trusted-users = [ "root" "@wheel" ];
 
-    security.sudo.wheelNeedsPassword = false;
+  security.sudo.wheelNeedsPassword = false;
 
-    # Enable SSH everywhere
-    services.openssh.enable = true;
+  # Enable SSH everywhere
+  services.openssh.enable = true;
 
-    # No need for sound on a server
-    sound.enable = false;
+  # No need for sound on a server
+  sound.enable = false;
 
-    # UTC everywhere!
-    time.timeZone = lib.mkDefault "UTC";
+  # UTC everywhere!
+  time.timeZone = lib.mkDefault "UTC";
 
-    # No mutable users by default
-    users.mutableUsers = false;
+  # No mutable users by default
+  users.mutableUsers = false;
 
-    systemd = {
-      # Given that our systems are headless, emergency mode is useless.
-      # We prefer the system to attempt to continue booting so
-      # that we can hopefully still access it remotely.
-      enableEmergencyMode = false;
+  systemd = {
+    # Given that our systems are headless, emergency mode is useless.
+    # We prefer the system to attempt to continue booting so
+    # that we can hopefully still access it remotely.
+    enableEmergencyMode = false;
 
-      # For more detail, see:
-      #   https://0pointer.de/blog/projects/watchdog.html
-      watchdog = {
-        # systemd will send a signal to the hardware watchdog at half
-        # the interval defined here, so every 10s.
-        # If the hardware watchdog does not get a signal for 20s,
-        # it will forcefully reboot the system.
-        runtimeTime = "20s";
-        # Forcefully reboot if the final stage of the reboot
-        # hangs without progress for more than 30s.
-        # For more info, see:
-        #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
-        rebootTime = "30s";
-      };
-
-      sleep.extraConfig = ''
-        AllowSuspend=no
-        AllowHibernation=no
-      '';
+    # For more detail, see:
+    #   https://0pointer.de/blog/projects/watchdog.html
+    watchdog = {
+      # systemd will send a signal to the hardware watchdog at half
+      # the interval defined here, so every 10s.
+      # If the hardware watchdog does not get a signal for 20s,
+      # it will forcefully reboot the system.
+      runtimeTime = "20s";
+      # Forcefully reboot if the final stage of the reboot
+      # hangs without progress for more than 30s.
+      # For more info, see:
+      #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
+      rebootTime = "30s";
     };
 
-    # use TCP BBR has significantly increased throughput and reduced latency for connections
-    boot.kernel.sysctl = {
-      "net.core.default_qdisc" = "fq";
-      "net.ipv4.tcp_congestion_control" = "bbr";
-    };
-  } // lib.optionalAttrs (options.environment ? stub-ld) {
-    # Don't install the /lib/ld-linux.so.2 and /lib64/ld-linux-x86-64.so.2
-    # stubs. Server users should know what they are doing.
-    environment.stub-ld.enable = lib.mkDefault false;
+    sleep.extraConfig = ''
+      AllowSuspend=no
+      AllowHibernation=no
+    '';
+  };
+
+  # use TCP BBR has significantly increased throughput and reduced latency for connections
+  boot.kernel.sysctl = {
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_congestion_control" = "bbr";
   };
 }
