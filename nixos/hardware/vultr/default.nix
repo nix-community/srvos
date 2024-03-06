fda52d3209d79196bb2588ae223591793b163b8b
zimbatm <zimbatm@zimbatm.com>
2024-03-06 13:23:29 +0100

    vultr: disable srvos.boot.consoles
    Reviewed-By: mic92

--- nixos/hardware/vultr/default.nix
+++ nixos/hardware/vultr/default.nix
@@ -1,8 +1,11 @@
-{
+{ lib, ... }: {
   imports = [
     ../../mixins/cloud-init.nix
   ];
 
   services.cloud-init.settings.datasource_list = [ "Vultr" ];
   services.cloud-init.settings.datasource.Vultr = { };
+
+  # tty1 is used by all of the servers so we don't want a serial console
+  srvos.boot.consoles = lib.mkDefault [ ];
 }
