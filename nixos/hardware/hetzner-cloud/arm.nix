8a4c3ea6458228fdadbd81ce27521702eeb92973
Enno Richter <enno@nerdworks.de>
2024-04-02 12:35:00 +0200

    hetzner-cloud/arm: remove boot.loader.timeout

--- nixos/hardware/hetzner-cloud/arm.nix
+++ nixos/hardware/hetzner-cloud/arm.nix
@@ -6,7 +6,6 @@
   config = {
     # arm uses EFI, so we need systemd-boot
     boot.loader.systemd-boot.enable = true;
-    boot.loader.timeout = 30;
     # since it's a vm, we can do this on every update safely
     boot.loader.efi.canTouchEfiVariables = true;
 
