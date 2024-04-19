d8945920cb8e98dc737d1fc2d42607f5916c34cf
Jörg Thalheim <Mic92@users.noreply.github.com>
2024-04-19 15:31:53 +0200

    mdns: don't enable it if we already have avahi (#417)

--- nixos/mixins/mdns.nix
+++ nixos/mixins/mdns.nix
@@ -1,7 +1,10 @@
-{ lib, ... }: {
-  # Allows to find machines on the local network by name, i.e. useful for printer discovery
-  systemd.network.networks."99-ethernet-default-dhcp".networkConfig.MulticastDNS = lib.mkDefault "yes";
-  systemd.network.networks."99-wireless-client-dhcp".networkConfig.MulticastDNS = lib.mkDefault "yes";
-  networking.firewall.allowedUDPPorts = [ 5353 ]; # Multicast DNS
+{ config, lib, ... }:
+{
+  # Avahi is an alternative implementation. If it's enabled, than we don't need the code below.
+  config = lib.mkIf (!config.services.avahi.enable) {
+    # Allows to find machines on the local network by name, i.e. useful for printer discovery
+    systemd.network.networks."99-ethernet-default-dhcp".networkConfig.MulticastDNS = lib.mkDefault "yes";
+    systemd.network.networks."99-wireless-client-dhcp".networkConfig.MulticastDNS = lib.mkDefault "yes";
+    networking.firewall.allowedUDPPorts = [ 5353 ]; # Multicast DNS
+  };
 }
-
