14bf076badd37b319e51a7b60101eb791d093b78
Jörg Thalheim <joerg@thalheim.io>
2024-06-27 12:55:29 +0200

    darwin: add zsh integration by default

--- darwin/common/default.nix
+++ darwin/common/default.nix
@@ -1,8 +1,11 @@
-{
-
+{ lib, ... }: {
   imports = [
     ./flake.nix
     ./nix.nix
     ./openssh.nix
   ];
+
+  # It's the default login shell, and if not enabled, a lot of important configuration is not applied correctly
+  # Overhead is minimal, since it's just generated zsh configuration that gets added.
+  programs.zsh.enable = lib.mkDefault true;
 }
