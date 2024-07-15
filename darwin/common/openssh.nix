9c9db0b387e7919ec13d6f4d762d4088c0c2d180
Harsh Shandilya <me@msfjarvis.dev>
2024-07-15 11:13:42 +0530

    darwin: drop removed services.openssh.authorizedKeysFiles option
    nix-darwin no longer allow to set this

--- darwin/common/openssh.nix
+++ darwin/common/openssh.nix
@@ -12,6 +12,4 @@
     # Use key exchange algorithms recommended by `nixpkgs#ssh-audit`
     KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,sntrup761x25519-sha512@openssh.com
   '';
-  # Only allow system-level authorized_keys to avoid injections.
-  services.openssh.authorizedKeysFiles = lib.mkForce [ "/etc/ssh/authorized_keys.d/%u" ];
 }
