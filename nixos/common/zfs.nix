460294b6dca9270b518542b8aa28fe19f6d5d011
zimbatm <zimbatm@zimbatm.com>
2024-07-15 17:47:46 +0200

    feat(zfs): set default networking.hostId
    In server scenario, disks are not getting shared, this is just not a
    thing. Don't force users to set this value.

--- nixos/common/zfs.nix
+++ nixos/common/zfs.nix
@@ -3,6 +3,9 @@
   # You may also find this setting useful to automatically set the latest compatible kernel:
   #boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 
+  # Use the same default hostID as the NixOS install ISO and nixos-anywhere.
+  networking.hostId = lib.mkDefault "8425e349";
+
   services.zfs = lib.mkIf (config.boot.zfs.enabled) {
     autoSnapshot.enable = true;
     # defaults to 12, which is a bit much given how much data is written
