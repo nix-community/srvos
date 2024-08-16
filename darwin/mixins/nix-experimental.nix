4bd6844d67252a6d7ff6ec3a8bd1f94e87857b58
Jörg Thalheim <joerg@thalheim.io>
2024-08-16 08:03:32 +0200

    nix-experimental: enable ca-derivations and impure-derivations

--- darwin/mixins/nix-experimental.nix
+++ darwin/mixins/nix-experimental.nix
@@ -15,8 +15,19 @@
 
       # Allow derivation builders to call Nix, and thus build derivations recursively.
       "recursive-nix"
-    ]
-    ++ lib.optional (lib.versionAtLeast (lib.versions.majorMinor config.nix.package.version) "2.19")
+
       # Allow the use of the impure-env setting.
-      "configurable-impure-env";
+      "configurable-impure-env"
+    ]
+    ++ lib.optionals (lib.versionAtLeast (lib.versions.majorMinor config.nix.package.version) "2.28") [
+      # Allow derivations to be content-addressed in order to prevent rebuilds
+      # when changes to the derivation do not result in changes to the
+      # derivation's output.
+      "ca-derivations"
+
+      # Allow derivations to produce non-fixed outputs by setting the __impure
+      # derivation attribute to true. An impure derivation can have differing
+      # outputs each time it is built.
+      "impure-derivations"
+    ];
 }
--- nixos/mixins/nix-experimental.nix
+++ nixos/mixins/nix-experimental.nix
@@ -19,10 +19,20 @@
 
       # Allow derivation builders to call Nix, and thus build derivations recursively.
       "recursive-nix"
-    ]
-    ++ lib.optional (lib.versionAtLeast (lib.versions.majorMinor config.nix.package.version) "2.19")
       # Allow the use of the impure-env setting.
-      "configurable-impure-env";
+      "configurable-impure-env"
+    ]
+    ++ lib.optionals (lib.versionAtLeast (lib.versions.majorMinor config.nix.package.version) "2.28") [
+      # Allow derivations to be content-addressed in order to prevent rebuilds
+      # when changes to the derivation do not result in changes to the
+      # derivation's output.
+      "ca-derivations"
+
+      # Allow derivations to produce non-fixed outputs by setting the __impure
+      # derivation attribute to true. An impure derivation can have differing
+      # outputs each time it is built.
+      "impure-derivations"
+    ];
 
   # no longer need to pre-allocate build users for everything
   nix.settings.auto-allocate-uids = true;
