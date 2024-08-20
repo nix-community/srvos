e8d5cd72ac36e767680536badf763a71c5e478be
zowoq <59103226+zowoq@users.noreply.github.com>
2024-08-20 11:13:38 +1000

    dev/private/flake: add nixpkgs-dev input

--- dev/private/flake.nix
+++ dev/private/flake.nix
@@ -1,12 +1,16 @@
 {
   description = "srvos private inputs";
 
+  # follows the same channel as nixpkgs in the main flake
+  inputs.nixpkgs-dev.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+
   inputs.nixos-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
 
   inputs.nix-darwin.url = "github:LnL7/nix-darwin";
   inputs.nix-darwin.inputs.nixpkgs.follows = "";
 
   inputs.mkdocs-numtide.url = "github:numtide/mkdocs-numtide";
+  inputs.mkdocs-numtide.inputs.nixpkgs.follows = "nixpkgs-dev";
 
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
   inputs.treefmt-nix.inputs.nixpkgs.follows = "";
@@ -17,7 +21,7 @@
   inputs.pre-commit-hooks-nix.inputs.nixpkgs-stable.follows = "";
 
   inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixos-stable";
+  inputs.flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs-dev";
 
   inputs.flake-compat.url = "github:nix-community/flake-compat";
 
