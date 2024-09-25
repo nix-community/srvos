8f05f88e6de4ea029ebe152c7555223c3432536f
Jörg Thalheim <joerg@thalheim.io>
2024-09-25 15:35:41 +0200

    only disable noXlibs for older nixpkgs version

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -28,14 +28,8 @@
   # Work around for https://github.com/NixOS/nixpkgs/issues/124215
   documentation.info.enable = false;
 
-  environment = {
-    # This is pulled in by the container profile, but it seems broken and causes
-    # unnecessary rebuilds.
-    noXlibs = false;
-    # Don't install the /lib/ld-linux.so.2 stub. This saves one instance of
-    # nixpkgs.
-    ldso32 = null;
-  };
+  # Don't install the /lib/ld-linux.so.2 stub. This saves one instance of nixpkgs.
+  environment.ldso32 = null;
 
   # Ensure a clean & sparkling /tmp on fresh boots.
   boot.tmp.cleanOnBoot = lib.mkDefault true;
