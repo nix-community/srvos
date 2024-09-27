5c17645462604f9a548a7c6733afa91fcae8c1a9
zowoq <59103226+zowoq@users.noreply.github.com>
2024-09-27 17:51:27 +1000

    add shared nix config

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -1,29 +1,15 @@
 { lib, config, ... }:
 {
+  imports = [
+    ../../shared/common/nix.nix
+  ];
+
   # Disable nix channels. Use flakes instead.
   nix.channel.enable = lib.mkDefault false;
-
-  # Fallback quickly if substituters are not available.
-  nix.settings.connect-timeout = lib.mkDefault 5;
-
-  # Enable flakes
-  nix.settings.experimental-features =
-    [
-      "nix-command"
-      "flakes"
-    ]
-    ++ lib.optional (lib.versionOlder (lib.versions.majorMinor config.nix.package.version) "2.22") "repl-flake";
-
-  # The default at 10 is rarely enough.
-  nix.settings.log-lines = lib.mkDefault 25;
 
   # De-duplicate store paths using hardlinks except in containers
   # where the store is host-managed.
   nix.optimise.automatic = lib.mkDefault (!config.boot.isContainer);
-
-  # Avoid disk full issues
-  nix.settings.max-free = lib.mkDefault (3000 * 1024 * 1024);
-  nix.settings.min-free = lib.mkDefault (512 * 1024 * 1024);
 
   # TODO: cargo culted.
   nix.daemonCPUSchedPolicy = lib.mkDefault "batch";
@@ -40,7 +26,4 @@
   # 100 is the default for user slices and 500 is systemd-coredumpd@
   # We rather want a build to be killed than our precious user sessions as builds can be easily restarted.
   systemd.services.nix-daemon.serviceConfig.OOMScoreAdjust = lib.mkDefault 250;
-
-  # Avoid copying unnecessary stuff over SSH
-  nix.settings.builders-use-substitutes = true;
 }
