c40e8ab950b1cc186a5b27c397ce0e0da4a23d9f
Sandro <sandro.jaeckel@gmail.com>
2024-10-02 17:18:51 +0200

    Also disable xdg.menus

--- nixos/server/default.nix
+++ nixos/server/default.nix
@@ -48,6 +48,7 @@
   # freedesktop xdg files
   xdg.autostart.enable = lib.mkDefault false;
   xdg.icons.enable = lib.mkDefault false;
+  xdg.menus.enable = lib.mkDefault false;
   xdg.mime.enable = lib.mkDefault false;
   xdg.sounds.enable = lib.mkDefault false;
 
