13219e8315fb7ca3390880aa6d76b349dddcf192
Sandro <sandro.jaeckel@gmail.com>
2024-10-02 17:14:14 +0200

    Disable docs, too

--- nixos/server/default.nix
+++ nixos/server/default.nix
@@ -36,6 +36,7 @@
 
   # Notice this also disables --help for some commands such es nixos-rebuild
   documentation.enable = lib.mkDefault false;
+  documentation.doc.enable = lib.mkDefault false;
   documentation.info.enable = lib.mkDefault false;
   documentation.man.enable = lib.mkDefault false;
   documentation.nixos.enable = lib.mkDefault false;
