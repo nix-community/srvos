e124e956a3f322aa48805c607544d45b3fbcebed
zowoq <59103226+zowoq@users.noreply.github.com>
2024-10-03 18:30:14 +1000

    darwin/server: various

--- darwin/server/default.nix
+++ darwin/server/default.nix
@@ -9,4 +9,14 @@
     # no config.programs.git.package option on darwin
     pkgs.git
   ];
+
+  # not disabled by the corresponding documentation.* option
+  programs.info.enable = lib.mkDefault false;
+  programs.man.enable = lib.mkDefault false;
+
+  # remove uninstaller, use 'nix run github:LnL7/nix-darwin#darwin-uninstaller'
+  system.includeUninstaller = lib.mkDefault false;
+
+  # UTC (GMT) everywhere!
+  time.timeZone = lib.mkDefault "GMT";
 }
