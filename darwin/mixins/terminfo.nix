a9f2ae9fb213b6175c71cd6aecfdb366979d2e0c
zowoq <59103226+zowoq@users.noreply.github.com>
2024-11-04 11:03:29 +1000

    darwin/mixins/terminfo: disable termite

--- darwin/mixins/terminfo.nix
+++ darwin/mixins/terminfo.nix
@@ -8,5 +8,7 @@
       pkgs.wezterm.terminfo # this one does not need compilation
       # avoid compiling desktop stuff when doing cross nixos
     ]
-    ++ lib.optionals (pkgs.stdenv.hostPlatform == pkgs.stdenv.buildPlatform) [ pkgs.termite.terminfo ];
+    ++ lib.optionals (pkgs.stdenv.hostPlatform == pkgs.stdenv.buildPlatform) [
+      # pkgs.termite.terminfo
+    ];
 }
