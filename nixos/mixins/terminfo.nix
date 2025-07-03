6bb452f0b31058ffe64241bcf092ebf1c7758be1
Florian Klink <flokli@flokli.de>
2025-07-03 20:42:10 +0300

    mixins/terminfo: add kitty (#661)
    This is hopefully as stable as ghostty by now.

--- nixos/mixins/terminfo.nix
+++ nixos/mixins/terminfo.nix
@@ -11,10 +11,9 @@
       # avoid compiling desktop stuff when doing cross nixos
     ]
     ++ lib.optionals (pkgs.stdenv.hostPlatform == pkgs.stdenv.buildPlatform) [
-      pkgs.termite.terminfo
-      # Too unstable
-      # pkgs.kitty.terminfo
       pkgs.foot.terminfo
       pkgs.ghostty.terminfo
+      pkgs.kitty.terminfo
+      pkgs.termite.terminfo
     ];
 }
