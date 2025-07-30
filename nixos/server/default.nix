8e7d3c690975ee6790926bdfd1258016c967d163
zowoq <59103226+zowoq@users.noreply.github.com>
2025-07-30 15:20:05 +1000

    nixos/server: systemd.watchdog -> systemd.settings.Manager
    https://github.com/NixOS/nixpkgs/commit/1adf0f56ff95cae9db9fb198950b5c10ba52635b

--- nixos/server/default.nix
+++ nixos/server/default.nix
@@ -83,28 +83,36 @@
     # that we can hopefully still access it remotely.
     enableEmergencyMode = false;
 
-    # For more detail, see:
-    #   https://0pointer.de/blog/projects/watchdog.html
-    watchdog = {
-      # systemd will send a signal to the hardware watchdog at half
-      # the interval defined here, so every 7.5s.
-      # If the hardware watchdog does not get a signal for 15s,
-      # it will forcefully reboot the system.
-      runtimeTime = lib.mkDefault "15s";
-      # Forcefully reboot if the final stage of the reboot
-      # hangs without progress for more than 30s.
-      # For more info, see:
-      #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
-      rebootTime = lib.mkDefault "30s";
-      # Forcefully reboot when a host hangs after kexec.
-      # This may be the case when the firmware does not support kexec.
-      kexecTime = lib.mkDefault "1m";
-    };
-
     sleep.extraConfig = ''
       AllowSuspend=no
       AllowHibernation=no
     '';
+  }
+  // lib.optionalAttrs (lib.versionAtLeast (lib.versions.majorMinor lib.version) "25.11") {
+    # For more detail, see:
+    #   https://0pointer.de/blog/projects/watchdog.html
+    settings.Manager = {
+      # systemd will send a signal to the hardware watchdog at half
+      # the interval defined here, so every 7.5s.
+      # If the hardware watchdog does not get a signal for 15s,
+      # it will forcefully reboot the system.
+      RuntimeWatchdogSec = lib.mkDefault "15s";
+      # Forcefully reboot if the final stage of the reboot
+      # hangs without progress for more than 30s.
+      # For more info, see:
+      #   https://utcc.utoronto.ca/~cks/space/blog/linux/SystemdShutdownWatchdog
+      RebootWatchdogSec = lib.mkDefault "30s";
+      # Forcefully reboot when a host hangs after kexec.
+      # This may be the case when the firmware does not support kexec.
+      KExecWatchdogSec = lib.mkDefault "1m";
+    };
+  }
+  // lib.optionalAttrs (lib.versionOlder (lib.versions.majorMinor lib.version) "25.11") {
+    watchdog = {
+      runtimeTime = lib.mkDefault "15s";
+      rebootTime = lib.mkDefault "30s";
+      kexecTime = lib.mkDefault "1m";
+    };
   };
 
   # Make sure the serial console is visible in qemu when testing the server configuration
