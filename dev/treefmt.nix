0070590bf5bd5dc97b8e644720c3c7c90e16f8bc
zowoq <59103226+zowoq@users.noreply.github.com>
2025-09-08 12:19:07 +1000

    treefmt: set no-lambda-arg for deadnix

--- dev/treefmt.nix
+++ dev/treefmt.nix
@@ -1,6 +1,7 @@
 {
   projectRootFile = ".git/config";
   programs.deadnix.enable = true;
+  programs.deadnix.no-lambda-arg = true;
   programs.deadnix.no-lambda-pattern-names = true;
   programs.nixfmt.enable = true;
   programs.shellcheck.enable = true;
