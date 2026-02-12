838e086ff78ccc287776f327a8ab54b6257067c2
phaer <phaer@phaer.org>
2026-02-12 11:03:03 +0100

    terminfo: fix ghostty directory hierachy

--- nixos/mixins/terminfo.nix
+++ nixos/mixins/terminfo.nix
@@ -14,8 +14,9 @@
       }
       ''
         7zz -snld x ${pkgs.ghostty-bin.src}
-        mkdir -p $out/share/terminfo
-        cp -r Ghostty.app/Contents/Resources/terminfo/* $out/share/terminfo/
+        mkdir -p $out/share/terminfo/{g,x}
+        cp -r Ghostty.app/Contents/Resources/terminfo/67/ghostty $out/share/terminfo/g
+        cp -r Ghostty.app/Contents/Resources/terminfo/78/xterm-ghostty $out/share/terminfo/x
       ''
     )
   ]
