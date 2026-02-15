98de0881ad9d5a35e25c1a2a1496fab1d77223f3
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2026-02-15 09:15:59 +0000

    darwin: use homebrew.prefix with nix-darwin rename

--- darwin/common/homebrew.nix
+++ darwin/common/homebrew.nix
@@ -7,7 +7,7 @@
   # Install homebrew if it is not installed
   system.activationScripts.homebrew.text = lib.mkIf config.homebrew.enable (
     lib.mkBefore ''
-      if [[ ! -f "${config.homebrew.brewPrefix}/brew" ]]; then
+      if [[ ! -f "${config.homebrew.prefix}/bin/brew" ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       fi
     ''
